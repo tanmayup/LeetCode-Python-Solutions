496d21922cb53e77433de4bc20fabaf2f7ee76a6
tanmayup <upadhyaytanmay50@gmail.com>
2026-07-21 02:48:21 +0530

    Time: 238 ms (73.84%), Space: 21.5 MB (71.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,27 @@
 class Solution:
-    ans = 0
-    def scan(self, grid, i, j):
-        rows = len(grid)
-        cols = len(grid[0])
-
-        if i < 0 or i >= rows or j < 0 or j >= cols:
+    def remove_neighbours(self, grid, i, j):
+        if i == -1 or i == len(grid) or j == -1 or j == len(grid[0]):
             return
+
+        # if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+        #     return
 
         if grid[i][j] == "0":
             return
 
         grid[i][j] = "0"
-
-        self.scan(grid, i+1, j)
-        self.scan(grid, i-1, j)
-        self.scan(grid, i, j+1)
-        self.scan(grid, i, j-1)
+        self.remove_neighbours(grid, i+1, j)
+        self.remove_neighbours(grid, i-1, j)
+        self.remove_neighbours(grid, i, j+1)
+        self.remove_neighbours(grid, i, j-1)
 
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-
         count = 0
-
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     count += 1
-                    self.scan(grid, i, j)
+                    self.remove_neighbours(grid, i, j)
 
         return count
