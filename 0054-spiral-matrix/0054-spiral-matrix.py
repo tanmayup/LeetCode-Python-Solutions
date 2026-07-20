c1b295a0c3842203c1d67e2fb4924c42d96bee0a
tanmayup <upadhyaytanmay50@gmail.com>
2026-07-21 01:41:34 +0530

    Time: 0 ms (100%), Space: 19.4 MB (33.91%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,26 +1,30 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        final, matrixc = [], []
-        matrixc[:] = matrix[:]
-        count = 0
-        print(matrix)
+        ans = []
+        while matrix:
+            if matrix:
+                for el in matrix[0]:
+                    ans.append(el)
+                matrix.pop(0)
 
-        while len(matrix) != 1:
-            for i in range(len(matrix[0])):
-                final.append(matrix[0][i])
+            if matrix:
+                for i in range(len(matrix)):
+                    ans.append(matrix[i][-1])
+                    matrix[i].pop(-1)
 
-            matrix = []
-            matrixc.pop(0)
+            matrix = [row for row in matrix if row]
 
-            for j in range(1, len(matrixc[0]) + 1):
-                newrow = []
-                for i in range(len(matrixc)):
-                    newrow.append(matrixc[i][(-1) * j])
-                matrix.append(newrow)
+            if matrix:
+                for i in range(len(matrix[-1])-1, -1, -1):
+                    ans.append(matrix[-1][i])
+                matrix.pop(-1)
 
-            matrixc[:] = matrix[:]
 
-        for el in matrix[0]:
-            final.append(el)
+            if matrix:
+                for i in range(1, len(matrix)+1):
+                    ans.append(matrix[-i][0])
+                    matrix[-i].pop(0)
 
-        return final
+            matrix = [row for row in matrix if row]
+
+        return ans
