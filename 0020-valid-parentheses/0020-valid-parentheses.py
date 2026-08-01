51a189eeb1819762ed7fa0a6afd4e0d9315c9be5
tanmayup <upadhyaytanmay50@gmail.com>
2026-08-01 19:15:16 +0530

    Time: 3 ms (32.29%), Space: 19.4 MB (24.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,57 +1,14 @@
-# class Stack:
-#     def __init__(self):
-#         self.st = []
-
-#     def push(self, x):
-#         self.st.append(x)
-
-#     def pop(self):
-#         if len(self.st) == 0:
-#             return -1
-
-#         x = self.st[-1]
-#         self.st.pop()
-#         return x
-    
-#     def top(self):
-#         if len(self.st) == 0:
-#             return -1
-        
-#         return self.st[-1]
-
-# class Solution():
-#     def isValid(self, s: str) -> bool:
-#         st = Stack()
-#         pairs = {")":"(",
-#                  "]":"[",
-#                  "}":"{"}
-#         for char in s:
-#             if char in pairs.values():
-#                 st.push(char)
-#             else:
-#                 if st.top() == pairs[char]:
-#                     st.pop()
-
-#                 else:
-#                     return False
-                
-#         return st.top() == -1
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-
-        opens = {'(', '[', '{'}
-        pairs = {")":"(", 
-        "]":"[", 
-        "}":"{"}
-
+        brackets = {")": "(", "}": "{", "]": "["}
+        st = []
         for char in s:
-            if char in opens:
-                stack.append(char)
+            if char in list(brackets.values()):
+                st.append(char)
             else:
-                if not stack or stack[-1] != pairs[char]:
+                if st and st[-1] == brackets[char]:
+                    st.pop()
+                else:
                     return False
-                stack.pop()
 
-        return len(stack) == 0
+        return len(st) == 0
