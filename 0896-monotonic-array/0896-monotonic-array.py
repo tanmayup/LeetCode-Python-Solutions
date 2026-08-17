9704f72592240d411c085b5b81266c38f3c4ca09
tanmayup <upadhyaytanmay50@gmail.com>
2026-08-18 03:09:48 +0530

    Time: 23 ms (85.28%), Space: 31.6 MB (25.98%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if nums[0] > nums[-1]:
+            for i in range(1, len(nums)):
+                if nums[i] > nums[i-1]:
+                    return False
+            return True
+
+        elif nums[0] < nums[-1]:
+            for i in range(1, len(nums)):
+                if nums[i] < nums[i-1]:
+                    return False
+            return True
+
+        else:
+            for i in range(1, len(nums)):
+                if nums[i] != nums[i-1]:
+                    return False
+            return True
