8a4fc98936c6059c53af48c68f683fad7c7f9a3e
tanmayup <upadhyaytanmay50@gmail.com>
2026-08-19 22:43:11 +0530

    Time: 0 ms (100%), Space: 19.2 MB (72.23%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -5,39 +5,54 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def traversal(self, root):
-        level = [root]
-        ans = []
+    # def traversal(self, root):
+    #     level = [root]
+    #     ans = []
 
-        if not root:
-            return []
+    #     if not root:
+    #         return []
 
-        while level:
-            nxt = []
-            for node in level:
-                if node:
-                    if node.left:
-                        nxt.append(node.left)
-                    else:
-                        nxt.append(None)
+    #     while level:
+    #         nxt = []
+    #         for node in level:
+    #             if node:
+    #                 if node.left:
+    #                     nxt.append(node.left)
+    #                 else:
+    #                     nxt.append(None)
                 
-                    if node.right:
-                        nxt.append(node.right)
-                    else:
-                        nxt.append(None)
+    #                 if node.right:
+    #                     nxt.append(node.right)
+    #                 else:
+    #                     nxt.append(None)
 
-            level_val = []
-            for node in level:
-                if node:
-                    level_val.append(node.val)
-                else:
-                    level_val.append(None)
+    #         level_val = []
+    #         for node in level:
+    #             if node:
+    #                 level_val.append(node.val)
+    #             else:
+    #                 level_val.append(None)
 
-            ans.append(level_val)
+    #         ans.append(level_val)
 
-            level = nxt
+    #         level = nxt
 
-        return ans
+    #     return ans
 
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return self.traversal(p) == self.traversal(q)
+        # return self.traversal(p) == self.traversal(q)
+
+        st = [(p, q)]
+
+        while st:
+            node1, node2 = st.pop()
+
+            if not node1 and not node2:
+                continue
+            elif None in [node1, node2] or node1.val != node2.val:
+                return False
+
+            st.append((node1.left, node2.left))
+            st.append((node1.right, node2.right))
+
+        return True
